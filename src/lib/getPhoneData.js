389dab4100b8881651be4832a0f46190c6fb9c57
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-25 15:32:47 +0600

    "updated vercel"

--- src/lib/getPhoneData.js
+++ src/lib/getPhoneData.js
@@ -1,8 +1,16 @@
 const getPhoneData = async () => {
   const res = await fetch("http://localhost:5000/api/v1/phone/get", {
-    cache: "force-cache",
+    next: {
+      revalidate: 3000,
+    },
   });
   return await res.json();
 };
 
 export default getPhoneData;
+
+// const res = await fetch("http://localhost:5000/api/v1/specialties", {
+//   //     next: {
+//   //       revalidate: 9000,
+//   //     },
+//   //   });
