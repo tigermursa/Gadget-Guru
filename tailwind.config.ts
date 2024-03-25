620a37df1a465d03c917552be99ba8b5b39b1b22
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-25 07:34:07 +0600

    "Contact us page done"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,9 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
-    //! Remember need to put the code vor vercel deploy
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+
   ],
   theme: {
     extend: {
